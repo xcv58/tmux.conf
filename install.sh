d956d7188c8dda9af66d1abd95d5fa35ff20a27f
xcv58 <chenyihonglove@gmail.com>
2015-04-04 16:04:11 -0400

    add install.sh for MacHome

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+SCRIPT_FILE=$0
+SCRIPT_PATH=$(dirname $SCRIPT_FILE)
+cd ${SCRIPT_PATH}
+
+unamestr=`uname`
+if [ "$unamestr" = 'Darwin' ]; then
+    lnCommand="ln -hf"
+    command -v reattach-to-user-namespace >/dev/null 2>&1 || brew install reattach-to-user-namespace
+else
+    lnCommand="ln -Pf"
+fi
+
+for config_file in ${PWD}/*.conf; do
+    link_command="${lnCommand} ${config_file} ${HOME}/.${config_file:t}"
+    echo ${link_command}
+    eval ${link_command}
+done
+
+link_command="ln -sf ${PWD}/tmux.start.sh /usr/local/bin/t"
+echo ${link_command}
+eval ${link_command}
