a0b4fe4e40eb73dd9773ba6b2bd26639ff301953
xcv58 <i@xcv58.com>
2018-08-09 17:53:07 -0700

    Refactor: _default has the same layout

--- tmux.start.sh
+++ tmux.start.sh
@@ -4,31 +4,47 @@ export PATH=$PATH:/usr/local/bin
 # abort if we're already inside a TMUX session
 [ "$TMUX" == "" ] || exit 0
 
-# startup a "default" session if none currently exists
-tmux has-session -t _default || tmux new-session -s _default -d
+create() {
+  tmux new -s "$1" -d
+  tmux split-window -v
+  tmux neww
+  tmux split-window -v
+  tmux select-window -t 1
+  tmux select-pane -t 1
+}
 
 connect() {
-    tmux attach-session -t "${@//-/ }"
-    exit
+  tmux attach-session -t "${@//-/ }"
+  exit
 }
 
 contains() {
-    for i in ${options[@]}
-    do
-        if [[ ${1} == ${i} ]]; then
-            return 0
-        fi
-    done
-    return 1
+  for i in "${options[@]}"
+  do
+    if [[ ${1} == "${i}" ]]; then
+      return 0
+    fi
+  done
+  return 1
 }
 
-options=($(tmux list-sessions -F "#S" | sed -e "s/ /-/g"))
-if [[ $# > 0 ]]; then
-    argument=$(echo ${*})
-    argument=${argument// /-}
-    contains ${argument} && connect ${argument} || echo ${argument}
-            contains ${options[$argument]} && connect ${options[$argument]} || connect ${options[0]}
-    exit
+connect_or_default() {
+  # shellcheck disable=SC2015
+  contains "$1" && connect "$1" || connect "${options[-1]}"
+}
+
+DEFAULT=_default
+# startup a "default" session if none currently exists
+tmux has-session -t "$DEFAULT" || create "$DEFAULT"
+
+# shellcheck disable=SC2046
+read -r -a options <<< $(tmux list-sessions -F '#S' | sed -e 's/ /-/g')
+if [[ $# -gt 0 ]]; then
+  argument="${*}"
+  argument=${argument// /-}
+  contains "${argument}" && connect "${argument}"
+  # shellcheck disable=SC2015
+  connect_or_default "${options[$argument]}"
 fi
 
 # present menu for user to choose which workspace to open
@@ -41,22 +57,17 @@ echo "------------------"
 echo " "
 select opt in "${options[@]}"
 do
-    case ${opt} in
-        "NEW SESSION")
-            read -p "Enter new session name: " SESSION_NAME
-            tmux new -s "$SESSION_NAME" -d
-            tmux split-window -v
-            tmux neww
-            tmux split-window -v
-            tmux select-window -t 1
-            tmux select-pane -t 1
-            tmux attach-session -t "$SESSION_NAME"
-            break;;
-        "zsh")
-            zsh --login
-            break;;
-        *)
-            contains ${opt} && connect ${opt} || connect ${options[0]}
-            break;;
-    esac
+  case ${opt} in
+    "NEW SESSION")
+      read -r -p "Enter new session name: " SESSION_NAME
+      create "$SESSION_NAME"
+      connect "$SESSION_NAME"
+      break;;
+    "zsh")
+      zsh --login
+      break;;
+    *)
+      connect_or_default "${opt}"
+      break;;
+  esac
 done
